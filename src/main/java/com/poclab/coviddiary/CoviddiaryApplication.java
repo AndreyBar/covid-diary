9a082d7f8338e038aba9185bbff13e79596d5732
Andrii Barabash <andrew.barbsh@gmail.com>
2020-11-21 12:02:05 +0200

    Initial setup

---
+++ src/main/java/com/poclab/coviddiary/CoviddiaryApplication.java
@@ -0,0 +1,13 @@
+package com.poclab.coviddiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoviddiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoviddiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poclab/coviddiary/CoviddiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poclab.coviddiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoviddiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
